b9918877b420314b714ec87d02896ec028dac3e3
好耶！ <86039582+cloudw233@users.noreply.github.com>
2025-10-06 11:21:52 +0800

    第一个commit捏

---
+++ modules/wife/__init__.py
@@ -0,0 +1,27 @@
+from core.builtins.bot import Bot
+from core.builtins.message.internal import I18NContext, Image, Plain
+from core.component import module
+from core.constants.path import assets_path
+from .database.models import TodayWifeInfo
+
+from datetime import datetime
+import os, random
+
+wif = module('wife', ["waifu","jrlp"], "{{I18N:wife.help}}")
+
+assets = assets_path / "modules" / "wife"
+
+wife_names = os.listdir(assets)
+
+@wif.command("{{I18N:wife.help}}")
+async def _(msg: Bot.MessageSession):
+    _id = msg.session_info.sender_id
+    random.seed(_id+str(datetime.now()))
+    chose = random.sample(wife_names, 1)[0]
+    db = await TodayWifeInfo(sender_id=_id, name=chose, count=0).get_or_none()
+    wife_now = "琪露诺.png"
+    if db:
+        wife_now = db.wife_name
+    if await TodayWifeInfo.get_wife(sender_id=_id, name=chose):
+        msg.finish([I18NContext("wife.message.success"), Plain(chose), Image(assets / chose)])
+    msg.finish([I18NContext("wife.message.failure"), Plain(wife_now), Image(assets / wife_now)])
---
+++ modules/wife/database/models.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+
+from tortoise import fields
+
+from core.database.base import DBModel
+
+table_prefix = "module_wife_"
+
+
+class TodayWifeInfo(DBModel):
+    """
+    用户随机到的老婆
+
+    :param sender_id: 用户 ID。
+    :param wife_name: 随机的老婆名。
+    :param count: 换老婆次数。
+    :param timestamp: 时间戳。
+    """
+    sender_id = fields.CharField(max_length=512, pk=True)
+    wife_name = fields.CharField(max_length=512)
+    count = fields.IntField()
+    timestamp = fields.DatetimeField(null=True, auto_now_add=True)
+
+    class Meta:
+        table = f"{table_prefix}info"
+
+    @classmethod
+    async def get_wife(cls, sender_id: str, name: str):
+        exist_info = await cls.get_or_none(sender_id=sender_id)
+        if exist_info and exist_info.timestamp.date() != datetime.now().date():
+            await exist_info.delete()
+        elif exist_info.count < 5:
+            exist_info.count += 1
+            await exist_info.save()
+            return True
+        else:
+            return False
+        new_info = (await cls.get_or_create(sender_id=sender_id, name=name, count=0))[0]
+        await new_info.save()
+        return True
+
