90ff8a0ba361f84d1e610db5e0dff9d10ddcdf70
好耶！ <86039582+cloudw233@users.noreply.github.com>
2025-10-08 11:51:50 +0800

    chore

--- modules/wife/__init__.py
+++ modules/wife/__init__.py
@@ -7,21 +7,42 @@ from .database.models import TodayWifeInfo
 from datetime import datetime
 import os, random
 
-wif = module('wife', ["waifu","jrlp"], "{I18N:wife.help}", developers=["haoye_qwq"])
+wif = module(
+    'wife',
+    {
+        "waifu": "wife",
+        "jrlp": "wife",
+        "hlp": "wife change"
+    },
+    "{I18N:wife.help}",
+    developers=["haoye_qwq"]
+)
 
 assets = assets_path / "modules" / "wife"
 
 wife_names = os.listdir(assets)
 
-@wif.command("{{I18N:wife.help}}")
-async def _(msg: Bot.MessageSession):
+
+async def waifu(msg: Bot.MessageSession, change: bool):
     _id = msg.session_info.sender_id
-    random.seed(_id+str(datetime.now()))
+    random.seed(_id + str(datetime.now()))
     chose = random.sample(wife_names, 1)[0]
     db = await TodayWifeInfo.get_or_none(sender_id=_id)
     wife_now = "琪露诺.png"
     if db:
         wife_now = db.wife_name
-    if await TodayWifeInfo.get_wife(sender_id=_id, name=chose):
-        await msg.finish([I18NContext("wife.message.success"), Plain(chose.split('.')[0]), Image(assets / chose)])
-    await msg.finish([I18NContext("wife.message.failure"), Plain(wife_now.split('.')[0]), Image(assets / wife_now)])
+    if change and db:
+        if await TodayWifeInfo.get_wife(sender_id=_id, name=chose):
+            await msg.finish([I18NContext("wife.message.success"), Plain(chose.split('.')[0]), Image(assets / chose)])
+        await msg.finish([I18NContext("wife.message.failure"), Plain(wife_now.split('.')[0]), Image(assets / wife_now)])
+    await msg.finish([Plain("你今天的老婆是"), Plain(chose.split('.')[0]), Image(assets / chose)])
+
+
+@wif.command("{{I18N:wife.help}}")
+async def _(msg: Bot.MessageSession):
+    await waifu(msg, False)
+
+
+@wif.command("change {换老婆}")
+async def _(msg: Bot.MessageSession):
+    await waifu(msg, True)
